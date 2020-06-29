93eaa193660684959f95e6a04ea8ffde6bb0461c
sist <sist@DESKTOP-MMETA3C>
2020-06-29 10:40:40 +0900

    MemberRecord.java

---
+++ mvcApp05/src/com/test/mvc/MemberRecord.java
@@ -0,0 +1,46 @@
+package com.test.mvc;
+
+public class MemberRecord
+{
+	private String id;
+	private int kor, eng, mat;
+	
+	// getter/ setter 구성
+	public String getId()
+	{
+		return id;
+	}
+	public void setId(String id)
+	{
+		this.id = id;
+	}
+	public int getKor()
+	{
+		return kor;
+	}
+	public void setKor(int kor)
+	{
+		this.kor = kor;
+	}
+	public int getEng()
+	{
+		return eng;
+	}
+	public void setEng(int eng)
+	{
+		this.eng = eng;
+	}
+	public int getMat()
+	{
+		return mat;
+	}
+	public void setMat(int mat)
+	{
+		this.mat = mat;
+	}
+
+	
+	
+	
+	
+}
