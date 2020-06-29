1153024c80794b7ed9c56c614ee028ae0fd6b731
NyeongB <kdtoaj14@ajou.ac.kr>
2020-06-29 11:33:21 +0900

    memberlist 추가 구현

---
+++ mvcApp05/src/com/test/mvc/MemberListController.java
@@ -0,0 +1,44 @@
+package com.test.mvc;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MemberListController implements Controller
+{
+	private IMemberDAO memberDAO;
+	
+	
+	public void setMemberDAO(IMemberDAO memberDAO)
+	{
+		this.memberDAO = memberDAO;
+	}
+
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		ModelAndView mav = new ModelAndView();
+		
+		ArrayList<Member> memberList = new ArrayList<Member>();
+		
+		
+		try
+		{
+			memberList = memberDAO.lists();
+			mav.addObject("memberList",memberList);
+			mav.setViewName("MemberTotalList");
+			
+		} catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		
+		return mav;
+	}
+
+}
